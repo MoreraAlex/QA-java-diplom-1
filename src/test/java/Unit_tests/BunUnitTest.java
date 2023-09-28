b23070b8268bd9290a43b1e7274cf703f55e1195
MoreraAlex <Moreraqa@gmail.com>
2023-09-28 13:54:56 +0300

    Юнит тесты - первая часть задания диплома.

--- src/test/java/Unit_tests/BunUnitTest.java
+++ src/test/java/Unit_tests/BunUnitTest.java
@@ -2,31 +2,54 @@ package Unit_tests;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import praktikum.Bun;
 
+@RunWith(Parameterized.class)
 public class BunUnitTest {
+    private Bun bun;
+    private final String expectedName;
+    private final float expectedPrice;
+
+
+    public BunUnitTest(String expectedName, float expectedPrice) {
+        this.expectedName = expectedName;
+        this.expectedPrice = expectedPrice;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] data() {
+        return new Object[][]{
+                {"zeroBun", 0f},
+                {"floatBun", 222.111f},
+                {"Regular Bun name and price", 1.99f},
+        };
+    }
+
+
 
     @Test
     public void bunConstructorTest() {
-        Bun cinnabon = new Bun("Синнабон", 2.99F);
+        bun = new Bun(expectedName, expectedPrice);
 
-        Assert.assertEquals("Имя не соответствует ожидаемому","Синнабон", cinnabon.getName());
-        Assert.assertEquals("Цена не соответствует ожидаемой", 2.99F, cinnabon.getPrice(), 0.001F);
+        Assert.assertEquals("Имя не соответствует ожидаемому",expectedName, bun.getName());
+        Assert.assertEquals("Цена не соответствует ожидаемой", expectedPrice, bun.getPrice(), 0.001F);
 
     }
 
     @Test
     public void getNameTest() {
-        Bun cinnabon = new Bun("Синнабон", 2.99F);
+        bun = new Bun(expectedName, expectedPrice);
 
-        Assert.assertEquals("Имя не соответствует ожидаемому","Синнабон", cinnabon.getName());
+        Assert.assertEquals("Имя не соответствует ожидаемому",expectedName, bun.getName());
     }
 
     @Test
     public void getPriceTest() {
-        Bun cinnabon = new Bun("Синнабон", 2.99F);
+        bun = new Bun(expectedName, expectedPrice);
 
-        Assert.assertEquals("Цена не соответствует ожидаемой", 2.99F, cinnabon.getPrice(), 0.001F);
+        Assert.assertEquals("Цена не соответствует ожидаемой", expectedPrice, bun.getPrice(), 0.001F);
     }
 
 
